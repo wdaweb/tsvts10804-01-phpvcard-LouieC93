340f9fc7bfdc55f2b1e799412fad65f7e8415dd5
LouieC93 <louieforcode@gmail.com>
2020-01-03 08:50:15 +0800

    Upload

---
+++ ans/index.php
@@ -0,0 +1,43 @@
+<style>
+  .main {
+    margin: 5% auto;
+    text-align: center;
+    width: 400px;
+  }
+  .data {
+    text-align: left;
+    padding: 0 20px;
+    border: #000 2px solid;
+    margin-bottom: 20px;
+  }
+  .data p{
+    min-width:300px;
+  }
+</style>
+
+<div class="main">
+  <h1>vCard產生器</h1>
+  <form action="api.php" method="post" enctype="multipart/form-data">
+    <div class="data">
+      <p>你的名字 | <input type="text" name="user" value="泰山下智久"></p>
+      <p>職位名稱 | <input type="text" name="title" value="網頁設計師"></p>
+      <p>
+        <span style="vertical-align:top">技能說明 | </span>
+        <textarea name="skill" cols=30 rows=10>HTML/CSS網頁概念基礎,PHP/MySQL資料處理,JS/JQ前端網頁設計,AI/PS平片影像處理,UI/UX設計規劃</textarea>
+      </p>
+      <p>email | <input type="text" name="mail" value="goldtimes5@gmail.com"></p>
+      <p>手機號碼 | <input type="text" name="phone" value="0988-588-088"></p>
+      <hr>
+      <p>人像照 | <input type="file" name="who"></p>
+      <p>版型選擇 |
+        <select name="layout">
+          <option value="green" selected>寶石綠</option>
+          <option value="pink">大地褐</option>
+          <option value="black">星空黑</option>
+          <option value="white">雲彩白</option>
+        </select>
+      </p>
+    </div>
+    <input type="submit" value="送出預覽">
+  </form>
+</div>
